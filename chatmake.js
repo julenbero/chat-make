29190a3422a59e6503f36579b4eec0845bee868d
julenbero <julenbero@gmail.com>
2020-08-20 22:18:54 -0500

    Selecion Canales y lectura de mensajes

--- chatmake.js
+++ chatmake.js
@@ -1,6 +1,8 @@
 const $chat = document.querySelector('.chat');
 const $input = document.querySelector('#text-message');
 const $send = document.querySelector('form');
+const $numch = document.querySelectorAll('li');
+let currentchannel = 0;
 const dummy = [{
     	author: {
     		image: 'http://lorempixel.com/40/40/animals/',
@@ -28,24 +30,36 @@ const dummy = [{
 const channels = [
     {
         messages: [],
-        name: 'channel1'
+        name: 'Channel 1'
     },
     {
         messages: [],
-        name: 'channel2'
+        name: 'Channel 2'
     },
     {
         messages: [],
-        name: 'channel3'
+        name: 'Channel 3'
     },
     {
         messages: [],
-        name: 'channel4'
+        name: 'Channel 4'
     }
 
 ];
 
-const channel = channels.find((channel) => channel.name === 'channel1');
+let channel = channels.find((channel) => '#' + channel.name === $numch[currentchannel].textContent);
+
+$numch.forEach((element, index) => {
+    element.addEventListener('click', () => {
+        const actual = document.getElementsByClassName('seleccion');
+        actual[0].setAttribute('class', '');
+        element.setAttribute('class', 'seleccion');
+        channel = channels.find((channel) => channel.name === element.textContent);
+        currentchannel = index;
+        $chat.innerHTML = "";
+        printMessages();
+    });
+});
 
 $send.addEventListener('submit', (sendMessage) => {
     sendMessage.preventDefault();
@@ -69,6 +83,7 @@ $send.addEventListener('submit', (sendMessage) => {
     channel.messages.push(message);
     $chat.scrollTop = $chat.scrollHeight;
     $input.value = "";
+    console.log(channels[currentchannel].messages);
     }
 });   
 
@@ -82,13 +97,14 @@ function addMessage(message) { //Se puede reemplazar message {author, time, data
                 </div>`;
     //console.log(template);
     $chat.innerHTML += template;
+    // Se puede usar map para reemplazar forEach
 }
 
-// for (i = 0; i < messages.length; i++) {
-//     addmessage(messages[i]);
-// }
+function printMessages(){
+    const messages = channels[currentchannel].messages;
+    messages.forEach((message) => {
+        addMessage(message);
+    });
+}
 
-// Se puede usar map para reemplazar forEach
-channel.messages.forEach((message) => {
-    addMessage(message);
-});
+
